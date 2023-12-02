b7b3fa4f2bb4c3b54e9e8091e603274b2aa631c0
Ainazik <sekinkyz@gmail.com>
2023-12-02 23:24:15 +0600

    jrjflrjf

--- src/Easy.java
+++ src/Easy.java
@@ -1,2 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Easy {
+    public static void main(String[] args) {
+        int[] aoa = {5,5,5,10,20};
+
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+            return digits;
+        }
+
+    return   digits;}
+
 }
